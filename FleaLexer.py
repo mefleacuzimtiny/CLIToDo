8d4535af0600e09d649f423e1f01bda358a3db63
MeFleaCuzImTiny <zurail18@protonmail.com>
2022-10-08 00:30:38 +0500

    Made FleaLexer fully operational

--- FleaLexer.py
+++ FleaLexer.py
@@ -1,4 +1,5 @@
-text = '/add 5'
+text = '/add 5 newvalue:"ass ass ass" sub'
+text += '\n'
 prefix = '/'
 
 
@@ -8,45 +9,61 @@ kind: str = ""
 inquotes = False
 
 KEYWORDS = {
-    "add": "ADD" 
+    "newvalue": "NEWVALUE",
+    "add": "ADD",
+    "sub": "SUBTRACT"
 }
-for pos, char in enumerate(text):
-    match char:
-        case prefix if pos == 0 and inquotes == False:
-            kind = "PREFIX"
 
-        case ':':
-            if inquotes == False:
-                kind = "COLON"
+for pos, char in enumerate(text, start = 1):
+    match char:
+        case prefix if pos == 1 and inquotes == False:
+            t_len += 1
+            kind = "PREFIX"
+            token_txt = text[pos - t_len: pos]
+            t_len = 0
+
+        case colon if char == ':' and inquotes == False:
+            t_len+=1
+            kind = "COLON"
+            token_txt = text[pos-t_len : pos]
+            t_len = 0
 
         case letter if letter.isalpha() and inquotes == False:
             t_len+=1
 
-            if not letter:                                                  #ERROR HERE!!!
+            if pos < len(text) and not text[pos].isalpha():                                                  #ERROR HERE!!!
                 token_txt = text[pos-t_len : pos]
-                
-                try:
+
+                if token_txt in KEYWORDS:
                     kind = KEYWORDS[token_txt]
-                except KeyError:
-                    if token_txt[0].isupper():
-                        kind = "BAREWORD"
-                    else:
-                        raise ValueError(f"Invalid Keyword {token_txt}")
+
+                if token_txt[0].isupper():
+                    kind = "BAREWORD"
+                        
+                if not (token_txt in KEYWORDS or token_txt[0].isupper()):
+                    raise ValueError(f"Invalid Keyword {token_txt}")
+
+                t_len = 0
         
         case '"':
             inquotes = not inquotes
+            
+        case substring if inquotes == True:
             t_len+=1
-            if pos <= len(text) and inquotes == False:
+            if pos < len(text) and text[pos] == '"':
                 kind = "SUBSTRING"
+                token_txt = text[pos - t_len: pos]
                 t_len = 0
-            if pos > len(text):
+            if pos == len(text):
                 raise ValueError("Unterminated String")
- 
+            
         case digit if digit.isdigit() and inquotes == False:
             t_len+=1
             if pos <= len(text):
                 kind = "NUMBER"
                 token_txt = text[pos-t_len: pos]
+                t_len = 0
 
-    print(kind)
+    if kind != None: print(kind, token_txt)
+    kind = None
         
